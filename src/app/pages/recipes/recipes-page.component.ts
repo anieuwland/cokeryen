1bb4e2106576224b269df4927c2b4b34752b4f6b
Arthur Nieuwland <anieuwland@nimmerfort.eu>
2024-07-19 16:56:39 +0200

    Ability to filter on ingredient

--- src/app/pages/recipes/recipes-page.component.ts
+++ src/app/pages/recipes/recipes-page.component.ts
@@ -14,8 +14,10 @@ import { CommonModule } from '@angular/common';
 })
 export class RecipesPage {
   @Input() bookRef: string | undefined = undefined;
-  @Input() tags: string | string[] | undefined = undefined;
-  _tags: string[] = [];
+  @Input() tag: string | string[] | undefined = undefined;
+  tags: string[] = [];
+  @Input() ingredient: string | string[] | undefined = undefined;
+  ingredients: string[] = [];
 
   _recipes: Recipe[] = [];
   recipes: [Recipe, Book][] = [];
@@ -30,16 +32,19 @@ export class RecipesPage {
   }
 
   ngOnChanges() {
-    this._tags = normalizeToArray(this.tags);
+    this.tags = normalizeToArray(this.tag);
+    this.ingredients = normalizeToArray(this.ingredient);
 
     const tagInArray = (tag: string, array: string[] | undefined) => (array ?? []).indexOf(tag) > -1
     const hasTags = (r: Recipe, tags: string[]) => tags.map((tag: string) => tagInArray(tag, r.modernized?.tags)).every(v => v);
+    const hasIngr = (r: Recipe, tags: string[]) => tags.map((tag: string) => tagInArray(tag, (r.modernized?.ingredients ?? []).map(v => v[1]))).every(v => v);
 
     const f1 = (r: Recipe) => this.bookRef === undefined || r.bookRef === this.bookRef;
-    const f2 = (r: Recipe) => this._tags.length > 0 ? hasTags(r, normalizeToArray(this.tags)) : true;
-    this.recipes = this._recipes.filter(f1).filter(f2).map(r => [r, this.books[r.bookRef]])
+    const f2 = (r: Recipe) => this.tags.length > 0 ? hasTags(r, this.tags) : true;
+    const f3 = (r: Recipe) => this.ingredients.length > 0 ? hasIngr(r, this.ingredients) : true;
+    this.recipes = this._recipes.filter(f1).filter(f2).filter(f3).map(r => [r, this.books[r.bookRef]])
 
-    this._recipes.filter(f1).forEach(r => console.warn(r.modernized?.tags, this.tags, normalizeToArray(this.tags), hasTags(r, normalizeToArray(this.tags))));
+    this._recipes.filter(f1).forEach(r => console.warn(r.modernized?.tags, this.tag, normalizeToArray(this.tag), hasTags(r, normalizeToArray(this.tag))));
   }
 }
 
