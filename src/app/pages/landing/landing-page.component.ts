60f681617ef84ea8ddca1863a8aa765ecf60b9d0
Arthur Nieuwland <anieuwland@nimmerfort.eu>
2024-09-16 08:33:23 +0200

    Various: 1) nicer, font-awesome home icon, 2) more toolbar buttons are actual anchor-links, 3) removed the useless author initials from recipe detials

--- src/app/pages/landing/landing-page.component.ts
+++ src/app/pages/landing/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { BookTeaserComponent } from '../../components/book-teaser/book-teaser.component';
 import { BookTeasersComponent } from "../../components/book-teasers/book-teasers.component";
@@ -9,17 +10,16 @@ import { RecipeBook } from '../../domain/recipe-book';
 import { RecipeEntry, RecipeImageUrlPipe, RecipeUrlPipe } from '../../domain/recipe-entry';
 import { LinkPreviewService } from '../../services/link-preview.service';
 import { PageContainerComponent } from "../../components/page-container/page-container.component";
-import { changeRecipeColumns, RecipesOverviewComponent } from "../../components/recipes-overview/recipes-overview.component";
+import { RecipesOverviewComponent } from "../../components/recipes-overview/recipes-overview.component";
 import { ResponsiveColumn } from "../../components/responsive-column/responsive-column.component";
 import { LikeLandingPage } from '../../domain/like';
-import { fromEvent, Observable, Subscription } from 'rxjs';
 
 export const LANDING_PAGE_TITLE = "Smaak van de redactie - Cokeryen";
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [BookTeaserComponent, BookTeasersComponent, CommonModule, RecipeTeaserComponent, ToolbarComponent, PageContainerComponent, RecipesOverviewComponent, RecipeImageUrlPipe, RecipeUrlPipe, ResponsiveColumn],
+  imports: [CommonModule, RouterModule, BookTeaserComponent, BookTeasersComponent, RecipeTeaserComponent, ToolbarComponent, PageContainerComponent, RecipesOverviewComponent, RecipeImageUrlPipe, RecipeUrlPipe, ResponsiveColumn],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
